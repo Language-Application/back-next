dbd8e005f71e2aae9f143dd1f74108574ac664e3
Lucas Briand <Lucasbriandbr@gmail.com>
2023-11-09 11:25:22 +0100

    Modifications du hyeadert

--- back/components/Header.js
+++ back/components/Header.js
@@ -32,19 +32,19 @@ export default function Header() {
 
   return (
 
-    <nav className="flex fixed top-0 items-center justify-center bg-white px-4 w-full z-10 h-[70px] border-gray-200 border-b-[2px]">
+    <nav className={`flex fixed top-0 items-center justify-center bg-white px-4 w-full z-10 h-[70px] transition-all duration-300 ease-in-out border-b-[2px] ${scroll>0?'border-gray-200':'border-white'}`}>
       
-      <div className={`flex flex-row items-center w-full max-w-[calc(1000px-64px)] ${isScrolled?'justify-between':'justify-center'}`}>
+      <div className={`flex flex-row items-center w-full max-w-[calc(1000px-64px)] ${isScrolled?'justify-between':'justify-center'} md:justify-between`}>
         
         <div className='flex flex-row'>
 
           <img src="/duo.png" alt="logo" className="h-8 w-8 mr-2"/>
           
-          <h1 className={`text-3xl font-bold text-green-600 ${isScrolled?'hidden':''}`}>deskiñ</h1>
+          <h1 className={`text-3xl font-bold text-green-600 md:flex ${isScrolled?'hidden':''}`}>deskiñ</h1>
 
         </div>
 
-        <button className={`flex text-sm flex-row px-4 py-2.5 items-center justify-center h-[50px] rounded-[15px] border-[2px] border-b-[4px] bg-green-500 border-green-600 text-white font-bold ${isScrolled?'':'hidden'}`}>
+        <button className={`text-sm flex-row px-4 py-2.5 items-center justify-center h-[50px] rounded-[15px] border-[2px] border-b-[4px] bg-green-500 border-green-600 text-white font-bold md:flex ${isScrolled?'':'hidden'}`}>
             
           <p>C'EST PARTI !</p>
 
